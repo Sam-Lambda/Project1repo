2d156250253dfe9271ef18084c658cd7a9867530
Sam-Lambda <samer.bishara@mail.mcgill.ca>
2021-12-24 14:15:21 -0500

    added floor level

--- DOMmodifier.js
+++ DOMmodifier.js
@@ -11,6 +11,14 @@ document.addEventListener('DOMContentLoaded', () => {
         gridDiv.appendChild(document.createElement("div"));
     }
 
+    // this is to solve freezing, look at movedown
+    for (let i = 0; i < 10; i++) {
+        //set of divs to act as a floor and avoid clipping
+        let takenDiv = document.createElement("div");
+        takenDiv.className = "taken";
+        gridDiv.appendChild(takenDiv);
+    }
+
     const width = 10 //'nextLine'
     // we're going to use this as a means of jumping to bigger indexes
     // when constructing the tetris pieces.
@@ -74,70 +82,55 @@ document.addEventListener('DOMContentLoaded', () => {
     // here they are in beginning positions, but in the function they will be changed.
     let currentPosition = 4  //where first tetromino starts
     let currentRotation = 0
+    //let current = theTetrominoes[0][0]
 
     // randomly select tetromino with its first rotation.
-    let random = Math.floor(Math.random()*theTetrominoes.length)
+    let random = Math.floor(Math.random() * theTetrominoes.length)
     // I don't think I like this method of randomness?
-    
-    let current = theTetrominoes[random][currentRotation] 
+
+    let current = theTetrominoes[random][currentRotation]
     // first tetromino with its first rotation in the array
 
     //drawing tetrominos
-    function draw() { //for each item in the array we add a coloring class tetromino
-        currentRotation.forEach(index => {//we're coloring the divs for each tetromino
+    function draw() {
+        current.forEach(index => {
             squares[currentPosition + index].classList.add('tetromino')
-            //simply put this colors tetromino div by div
         })
     }
 
+    //draw()
+    //for each item in the array we add a coloring class tetromino
+    //we're coloring the divs for each tetromino
+    //simply put this colors tetromino div by div
+
     function undraw() {
-        currentRotation.forEach(index => {
+        current.forEach(index => {
             squares[currentPosition + index].classList.remove('tetromino')
         })
     } // remove div from grid
 
-
-
     // the time with which the tetromino move (fall) down, is, every second.
-    timerId = setInterval(moveDown, 1000)
+    timerId = setInterval(moveDown, 1000) //where is this even used though?
 
     // function to operate the fall of tetromino pieces
     function moveDown() {
         undraw()
         currentPosition += width //goes down one line
-        draw()
+        draw() // it goes down, but we must limit the grid so it doesn't clip out
         freeze() //invoking to check condition every second
-
-        // it goes down, but we must limit the grid so it doesn't clip out
     }
 
     // we solve the clipping with a freeze function
-    let takenDiv = document.createElement("div");
-    takenDiv.className = "taken";
-    body.appendChild(takenDiv);
-
-    for (let i = 0; i < 10; i++) {
-        takenDiv.appendChild(document.createElement("div"));
+    // if a tetromino runs into taken div, turn it to taken.
+    function freeze() {
+        if (current.some(index => squares[currentPosition + index + width].classList.contains('taken'))) {
+            current.forEach(index => squares[currentPosition + index].classList.add('taken'))
+            // immediately drop the next tetromino
+            random = Math.floor(Math.random() * theTetrominoes.length) // again idk if i like this
+            current = theTetrominoes[random][currentRotation]
+            currentPosition = 4
+            draw()
+        }
     }
-
-    function freeze() { // if a tetromino runs into taken div, turn it to taken.
-        if(current.some(index => squares[currentPosition + index + width].classList.contains('taken')))  {
-           current.forEach(index => squares[currentPosition + index].classList.add('taken'))
-           // immediately drop the next tetromino
-           random = Math.floor(Math.random() * theTetrominoes.length) // again idk if i like this
-           current = theTetrominoes[random][currentPosition]
-           currentPosition = 4
-           draw()
-    }
-}
-
-    
-
-
-
-
-
-
-
 
 })
